ac12d339160e76a34e448fc3ec51cb1478594f64
Carla Henriques <mail@carlahnr.com>
2024-10-03 00:50:14 +0100

    added new attribute "value".

--- engcia/VehicleDiagnosis/src/main/java/org/innov8/model/Hypothesis.java
+++ engcia/VehicleDiagnosis/src/main/java/org/innov8/model/Hypothesis.java
@@ -2,15 +2,22 @@ package org.innov8.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.innov8.vehicleDiagnosis.VehicleDiagnosis;
 
 @Getter
 @Setter
 public class Hypothesis extends Fact {
-    private String description;
 
-    public Hypothesis(String description) {
-        super();
+    private String description;
+    private String value;
+
+    public Hypothesis(String description, String value) {
         this.description = description;
+        this.value = value;
+        VehicleDiagnosis.agendaEventListener.addRhs(this);
     }
 
+    public String toString() {
+        return (description + " = " + value);
+    }
 }
