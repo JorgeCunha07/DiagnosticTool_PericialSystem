743dbdff30ecb6440fdcdc2dee4e30a5239246f0
Carla Henriques <mail@carlahnr.com>
2024-10-03 01:01:43 +0100

    refatorei nome dos atributos

--- engcia/VehicleDiagnosis/src/main/java/org/innov8/model/Evidence.java
+++ engcia/VehicleDiagnosis/src/main/java/org/innov8/model/Evidence.java
@@ -12,59 +12,61 @@ import java.lang.String;
 @NoArgsConstructor
 public class Evidence extends Fact{
 
+    // TODO mudar nomes das variaveis para uppercase
+
     // O carro apresenta algum problema
-    private String carIssue;
+    public static final String CAR_ISSUE = "O carro apresenta algum problema";
 
     // O carro não liga
-    private String carTurnsOn;
+    public static final String CAR_TURNS_ON = "O carro não liga";
 
     // Verificar a bateria (Bateria fraca ou sem carga?)
-    private String batteryCheck;
+    public static final String BATTERY_CHECK = "Bateria fraca ou sem carga";
 
     // Verificar motor de arranque (Motor de arranque com defeito?)
-    private String startingMotor;
+    public static final String STARTING_MOTOR = "Motor de arranque com defeito";
 
     // Verificar sistema de ignição (Ignição a falhar?)
-    private String ignitionSystem;
+    public static final String IGNITION_SYSTEM = "Ignição a falhar";
 
     // Verificar sistema de segurança (Imobilizador ou alarme a bloquear?)
-    private String securitySystem;
+    public static final String SECURITY_SYSTEM = "Imobilizador ou alarme a bloquear";
 
     // Verificar sistema de segurança (Imobilizador ou alarme a bloquear?)
-    private String alarmBlocked;
+    public static final String ALARM_BLOCKED = "Imobilizador ou alarme a bloquear";
 
     // O carro liga mas vai abaixo?
-    private String turnsOnAndShutsdown;
+    public static final String TURNS_ON_AND_SHUTSDOWN = "O carro liga mas vai abaixo";
 
     // Verificar superaquecimento do motor (Motor a sobreaquecer?)
-    private String engineOverheats;
+    public static final String ENGINE_OVERHEATS = "Motor a sobreaquecer";
 
     // Verificar sistema de arrefecimento (Radiador ou ventoinha com defeito?)
-    private String faultyRadiator;
+    public static final String FAULTY_RADIATOR = "Radiador com defeito";
 
-    private String faultyFan;
+    public static final String FAULTY_FAN = "Ventoinha com defeito";
 
     // Verificar fugas no sistema de arrefecimento (Fuga detectado?)
-    private String coolingSystemLeaks;
+    public static final String COOLING_SYSTEM_LEAKS = "Fuga no sistema de arrefecimento detectado";
 
     // Verificar termostato (Termostato defeituoso?)
-    private String faultyThermostat;
+    public static final String FAULTY_THERMOSTAT = "Termostato defeituoso";
 
     // Verificar sistema de combustível (Falta de combustível ou falha na bomba?)
-    private String fuelSystemIssue;
+    public static final String FUEL_SYSTEM_ISSUE = "Falta de combustível ou falha na bomba";
 
     // Verificar sistema de injeção de combustível (Injeção de combustível com defeito?)
-    private String fuelInjectionSystem;
+    public static final String FUEL_INJECTION_SYSTEM = "Injeção de combustível com defeito";
 
     // Verificar sistema de ignição (Velas de ignição defeituosas?)
-    private String faultySparkPlug;
+    public static final String FAULTY_SPARK_PLUG = "Velas de ignição defeituosas";
 
     private String evidence;
     private String value;
 
-    public Evidence(String ev, String v) {
-        evidence = ev;
-        value = v;
+    public String toString() {
+        return (evidence + " = " + value);
     }
+
 
 }
