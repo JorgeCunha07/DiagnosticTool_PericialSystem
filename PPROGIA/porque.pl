1a274f7be7891b5abb98228372233a488681620d
Moutinho <1140858@isep.ipp.pt>
2024-10-25 22:43:18 +0100

    Update porque.pl
    fix do erro da linha 6

--- PPROGIA/porque.pl
+++ PPROGIA/porque.pl
@@ -3,21 +3,22 @@
 % Exemplo: ?- porque(classe(meu_veiculo,ligeiro)).
 porque(Facto) :-
     (   facto(N, Facto)
-        -> N == 1 (
-            -> write('O facto '), write(Facto), write(' e verdadeiro visto ser a primeira pergunta')
-           Facto =.. [_, _, Resposta],
-           NAnterior is N-1,
-           justifica(NAnterior, ID, _),
-           facto(NAnterior, FactoAnterior),
-           FactoAnterior =.. [_, _, RespostaAnterior],
-           pergunta(FactoAnterior, PerguntaAnteriorFormatada),
-           write('O facto '), write(Facto),
-           write(' e verdadeiro porque pela regra '), write(ID),
-           write(' foi possivel concluir que o utilizador respondeu: '), write(RespostaAnterior),
-           write(' a pergunta: '), write(PerguntaAnteriorFormatada), nl,
-           write('Com isto foi necessario realizar a seguinte pergunta: '),
-           pergunta(Facto, PerguntaFormatada),
-           write(PerguntaFormatada), write(' ao qual o mesmo respondeu: '), write(Resposta),
-           write(' mostrando que o facto '), write(Facto), write(' e verdadeiro')
-        ; write('O facto '), write(Facto), write(' nao e verdadeiro')
+    ->  (N == 1
+        ->  write('O facto '), write(Facto), write(' é verdadeiro visto ser a primeira pergunta')
+        ;   Facto =.. [_, _, Resposta],
+            NAnterior is N - 1,
+            justifica(NAnterior, ID, _),
+            facto(NAnterior, FactoAnterior),
+            FactoAnterior =.. [_, _, RespostaAnterior],
+            pergunta(FactoAnterior, PerguntaAnteriorFormatada),
+            write('O facto '), write(Facto),
+            write(' é verdadeiro porque pela regra '), write(ID),
+            write(' foi possível concluir que o utilizador respondeu: '), write(RespostaAnterior),
+            write(' à pergunta: '), write(PerguntaAnteriorFormatada), nl,
+            write('Com isto foi necessário realizar a seguinte pergunta: '),
+            pergunta(Facto, PerguntaFormatada),
+            write(PerguntaFormatada), write(' ao qual o mesmo respondeu: '), write(Resposta),
+            write(' mostrando que o facto '), write(Facto), write(' é verdadeiro')
+        )
+    ;   write('O facto '), write(Facto), write(' não é verdadeiro')
     ).
