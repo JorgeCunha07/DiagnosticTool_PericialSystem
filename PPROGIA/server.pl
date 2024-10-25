9535bd4ef83d1ba3f610b8969bb213a7b576c529
Moutinho <1140858@isep.ipp.pt>
2024-10-25 23:32:22 +0100

    Update server.pl
    Handdler para o como e porque

--- PPROGIA/server.pl
+++ PPROGIA/server.pl
@@ -20,8 +20,10 @@ servidor(Port) :-
 :- http_handler(root(escolherCarro/motor), http_handler_listar_motores, [method(post)]).
 :- http_handler(root(obterNumeroCarro), http_hanlder_procurar_numero_carro, [method(post)]).
 :- http_handler(root(responder), responder_handler, [method(post)]).
+:- http_handler(root(porque), http_handler_porque, [method(post)]).
 :- http_handler(root(factos), factos_handler, [method(get)]).
 :- http_handler(root(pergunta), pergunta_handler, [method(get)]).
+:- http_handler(root(como), como_handler, [method(get)]).
 :- http_handler(root(diagnostico), diagnostico_handler, [method(get)]).
 
 log_message(Message) :-
@@ -77,6 +79,43 @@ http_handler_escolher_carro(Request) :-
     assertz(facto(1, proximo_teste(Numero, problemas))),
     reply_json(_{ carro_escolhido: Carro }).
 
+http_handler_porque(Request) :-
+    http_read_json_dict(Request, JsonIn),
+    atom_string(FactoAtom, JsonIn.facto),      % Convertendo o fato JSON em átomo
+    term_to_atom(FactoTerm, FactoAtom),        % Convertendo o átomo em termo Prolog
+    porque_response(FactoTerm, Explicacao),
+    reply_json(Explicacao).
+
+porque_response(Facto, Explicacao) :-
+    (   call(facto(N, Facto))
+    ->  (N == 1
+        ->  term_to_atom(Facto, FactoAtom),
+            Explicacao = _{fato: FactoAtom, explicacao: "O facto é verdadeiro visto ser a primeira pergunta"}
+        ;   Facto =.. [_, _, Resposta],
+            NAnterior is N - 1,
+            justifica(NAnterior, ID, _),
+            call(facto(NAnterior, FactoAnterior)),
+            FactoAnterior =.. [_, _, RespostaAnterior],
+            pergunta(FactoAnterior, PerguntaAnteriorFormatada),
+            pergunta(Facto, PerguntaFormatada),
+            % Converter termos compostos em átomos para JSON
+            term_to_atom(Facto, FactoAtom),
+            term_to_atom(Resposta, RespostaAtom),
+            term_to_atom(RespostaAnterior, RespostaAnteriorAtom),
+            Explicacao = _{
+                fato: FactoAtom,
+                explicacao: "O facto é verdadeiro",
+                regra: ID,
+                resposta_anterior: RespostaAnteriorAtom,
+                pergunta_anterior: PerguntaAnteriorFormatada,
+                pergunta: PerguntaFormatada,
+                resposta: RespostaAtom
+            }
+        )
+    ;   term_to_atom(Facto, FactoAtom),
+        Explicacao = _{fato: FactoAtom, explicacao: "O facto não é verdadeiro"}
+    ).
+
 procurar_carro(Numero, Carro) :-
     carro(Numero, Marca, Modelo, Motor),
     format(atom(Carro), '~w ~w ~w', [Marca, Modelo, Motor]).
@@ -96,6 +135,40 @@ pergunta_handler(_Request) :-
     functor(TesteTermo, Teste, 2),
     pergunta(TesteTermo, Pergunta),
     reply_json(Pergunta).
+
+
+como_handler(_Request) :-
+    como_response(1, Response),
+    reply_json(Response).
+
+como_response(N, Response) :-
+    NSeguinte is N + 1,
+    (   justifica(N, ID, _)
+    ->  facto(N, F),
+        F =.. [_, _, Regra],
+        pergunta(F, PerguntaFormatada),
+        % Transformar o termo composto em um átomo para JSON
+        term_to_atom(F, FAtom),
+        term_to_atom(Regra, RegraAtom),
+        % Criação de uma estrutura de resposta em JSON para a pergunta atual
+        Current = _{
+            pergunta: PerguntaFormatada,
+            resposta: RegraAtom,
+            fato: FAtom,
+            regra: ID
+        },
+        facto(NSeguinte, FSeguinte),
+        FSeguinte =.. [PerguntaSeguinte, _, RespostaSeguinte],
+        (   PerguntaSeguinte == diagnostico
+        ->  term_to_atom(RespostaSeguinte, DiagnosticoAtom),
+            Response = [Current, _{diagnostico: DiagnosticoAtom}]
+        ;   como_response(NSeguinte, NextResponse),
+            Response = [Current | NextResponse]
+        )
+    ;   Response = []
+    ).
+
+
         
 responder_handler(Request) :-
     % Verifica se há perguntas por responder
@@ -136,6 +209,8 @@ diagnostico_handler(_Request) :-
         solucao: Solucao
     },
     reply_json(Resposta).
+	
+
 
 % Utilidade para começar o servidor na porta 8080 (pode ser ajustada)
 :- servidor(8080).
